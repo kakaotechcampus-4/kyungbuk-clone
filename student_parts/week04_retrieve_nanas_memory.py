9c463ed142617e5b550ff37b113ccf981fcce74b
jungi Hong <hjg020531@naver.com>
2026-07-24 23:22:20 +0900

    fix: 미구현 툴 목록에서 제외

--- student_parts/week04_retrieve_nanas_memory.py
+++ student_parts/week04_retrieve_nanas_memory.py
@@ -375,7 +375,6 @@ def week04_tools() -> list[Any]:
         add_personal_reference,
         search_personal_references,
         search_saved_requests,
-        search_conversation_messages,
     ]
 
 
